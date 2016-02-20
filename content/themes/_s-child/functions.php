24547096722e7139ccd8612504ce7819611cc4de
Ramiro Azar <ramiroazar@gmail.com>
2016-02-20 19:27:08 +1000

    npm

--- content/themes/_s-child/functions.php
+++ content/themes/_s-child/functions.php
@@ -4,3 +4,11 @@
  * Normalize
  */
 require get_stylesheet_directory() . '/inc/normalize.php';
+
+/**
+ * Enqueue scripts and styles.
+ */
+function _s_scripts_child() {
+  wp_enqueue_script( '_s-javascript', get_stylesheet_directory_uri() . '/js/javascript.js', array('jquery'), false, true );
+}
+add_action( 'wp_enqueue_scripts', '_s_scripts_child' );
--- content/themes/_s-child/inc/normalize.php
+++ content/themes/_s-child/inc/normalize.php
@@ -3,46 +3,48 @@
 /**
  * functions.php
  */
-function _s_setup_child() {
+function _s_setup_normalize() {
   remove_theme_support( 'custom-background' );
 }
-add_action( 'after_setup_theme', '_s_setup_child', 15 );
+add_action( 'after_setup_theme', '_s_setup_normalize', 15 );
 
-function _s_scripts_child() {
-  wp_enqueue_style( '_s-style-parent', get_template_directory_uri() . '/style.css' );
+function _s_scripts_normalize() {
+  wp_enqueue_style( '_s-stylesheet', get_stylesheet_directory_uri() . '/stylesheet.css' );
+
+  wp_dequeue_style( '_s-style', get_stylesheet_uri() );
 
 	wp_dequeue_script( '_s-navigation' );
 
 	wp_dequeue_script( '_s-skip-link-focus-fix' );
 }
-add_action( 'wp_enqueue_scripts', '_s_scripts_child', 15 );
+add_action( 'wp_enqueue_scripts', '_s_scripts_normalize', 15 );
 
 /**
  * inc/custom-header.php
  */
-function _s_custom_header_setup_child() {
+function _s_custom_header_setup_normalize() {
   remove_theme_support( 'custom-header' );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup_child', 15 );
+add_action( 'after_setup_theme', '_s_custom_header_setup_normalize', 15 );
 
 /**
  * inc/customizer.php
  */
-function _s_customize_register_child() {
+function _s_customize_register_normalize() {
 	remove_action( 'customize_register', '_s_customize_register' );
 }
-add_action( 'init', '_s_customize_register_child' );
+add_action( 'init', '_s_customize_register_normalize' );
 
-function _s_customize_preview_js_child() {
+function _s_customize_preview_js_normalize() {
 	remove_action( 'customize_preview_init', '_s_customize_preview_js' );
 }
-add_action( 'init', '_s_customize_preview_js_child' );
+add_action( 'init', '_s_customize_preview_js_normalize' );
 
 /**
  * inc/jetpack.php
  */
-function _s_jetpack_setup_child() {
+function _s_jetpack_setup_normalize() {
   remove_theme_support( 'infinite-scroll' );
   remove_theme_support( 'jetpack-responsive-videos' );
 }
-add_action( 'after_setup_theme', '_s_jetpack_setup_child', 15 );
+add_action( 'after_setup_theme', '_s_jetpack_setup_normalize', 15 );
