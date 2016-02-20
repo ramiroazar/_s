93c7d8de438bbccd36f2ee1315506519509f8759
Ramiro Azar <ramiroazar@gmail.com>
2016-02-21 08:39:55 +1000

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,31 +10,34 @@ var path = require('path');
 var project_directory = path.basename(__dirname);
 var parent_theme = '_s';
 var child_theme = project_directory;
-var parent_theme_directory = 'content/themes/' + parent_theme + '/';
-var child_theme_directory = 'content/themes/' + child_theme + '/';
+var parent_theme_directory = 'content/themes/' + parent_theme;
+var child_theme_directory = 'content/themes/' + child_theme;
+var php_src = [
+  child_theme_directory + '/**/*.php'
+];
 var sass_src = [
-  child_theme_directory + 'sass/**/*.scss'
+  child_theme_directory + '/sass/**/*.scss'
 ];
 var css_src = [
-  parent_theme_directory + '*.css',
-  child_theme_directory + '!(stylesheet).css'
+  parent_theme_directory + '/*.css',
+  child_theme_directory + '/!(stylesheet).css'
 ];
 var javascript_src = [
-  parent_theme_directory + 'js/**/!(customizer).js',
-  child_theme_directory + 'js/**/!(javascript).js'
+  parent_theme_directory + '/js/**/!(customizer).js',
+  child_theme_directory + '/js/**/!(javascript).js'
 ];
 
 gulp.task('javascript', function() {
   return gulp.src(javascript_src)
     .pipe(concat('javascript.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(child_theme_directory + 'js'))
+    .pipe(gulp.dest(child_theme_directory + '/js'))
 });
 
 gulp.task('sass', function() {
   return gulp.src(sass_src)
     .pipe(compass({
-      sass: child_theme_directory + 'sass',
+      sass: child_theme_directory + '/sass',
       css: child_theme_directory,
       require: ['susy']
     }))
@@ -55,6 +58,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('watch', ['server', 'javascript', 'sass', 'css'], function (){
+  gulp.watch(php_src, [browserSync.reload]);
   gulp.watch(javascript_src, ['javascript', browserSync.reload]);
   gulp.watch(sass_src, ['sass']);
   gulp.watch(css_src, ['css', browserSync.reload]);
