6ce6c801057b0b803ad02590395369050cfb71ce
Ramiro Azar <ramiroazar@gmail.com>
2016-07-28 17:24:38 +1000

    Changed maximum allowed width variable

--- content/themes/underscores/inc/normalize.php
+++ content/themes/underscores/inc/normalize.php
@@ -22,6 +22,18 @@ function _s_setup_normalize() {
 }
 add_action( 'after_setup_theme', '_s_setup_normalize', 15 );
 
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function _s_content_width_normalize() {
+	$GLOBALS['content_width'] = apply_filters( '_s_content_width_normalize', 768 );
+}
+add_action( 'after_setup_theme', '_s_content_width_normalize', 15 );
+
 function _s_widgets_init_normalize() {
 	unregister_sidebar( 'sidebar-1' );
 }
